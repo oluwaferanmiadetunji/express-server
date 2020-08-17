8e621ba65756020b18b0f351847d6a9410487569
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-08-17 12:52:38 +0100

    Get port number from user

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-exports.printMsg = function () {
-	console.log('This is a message from the demo package');
-};
+const readline = require('readline');
+const {DEFAULT_PORT} = require('./constants');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+let port;
+
+rl.question('What port do you want to run it on? (5000) ', (portInput) => {
+	const pattern = /^\d+$/;
+	if (portInput) {
+		if (pattern.test(portInput)) {
+			port = portInput;
+		} else {
+			console.log('The port must be of type: Integer');
+		}
+	} else {
+		port = DEFAULT_PORT;
+	}
+
+	rl.close();
+});
+
+rl.on('close', function () {
+	console.log(`Your server will run on port ${port}`);
+	process.exit(0);
+});
