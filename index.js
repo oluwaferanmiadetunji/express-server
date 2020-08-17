68a4aefb40b98e361a342df92c7d48014c2b6e8d
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-08-17 20:33:47 +0100

    Add other input details

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ import {DEFAULT_PORT} from './constants.js';
 
 const rl = createInterface(process.stdin, process.stdout);
 
-let port, projectName;
+let port, name, description, author;
 
-rl.question('What is your project name: ', (name) => {
-	rl.question('What port do you want to run it on? (5000) ', (portInput) => {
-		projectName = name.toLowerCase();
-		port = portInput ? portInput : DEFAULT_PORT;
-		rl.close();
+rl.question('What is your project name: ', (projectName) => {
+	rl.question('What port do you want to run it on? (5000) ', (projectPort) => {
+		rl.question('Project Author: ', (projectAuthor) => {
+			rl.question('Project Description: ', (projectDescription) => {
+				name = projectName.trim().toLowerCase();
+				port = projectPort ? projectPort : DEFAULT_PORT;
+				description = projectDescription ? projectDescription.trim() : '';
+				author = projectAuthor ? projectAuthor.trim() : '';
+				rl.close();
+			});
+		});
 	});
 });
 
 rl.on('close', function () {
-	console.log(`Your project (${projectName})'s server will run on port ${port}`);
+	console.log(`Your project (${name})'s server will run on port ${port}, author: ${author}, description: ${description}`);
 	process.exit();
 });
