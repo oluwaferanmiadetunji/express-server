e5a7560079c4bc658a5a1b49a8ac7a511cbdf3b1
Adetunji Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-08-18 01:19:36 +0100

    Finish project

--- constants.js
+++ constants.js
@@ -1,4 +1,3 @@
 export const DEFAULT_PORT = 5000;
 export const DEFAULT_NAME = 'my-app';
 export const FOLDER_EXISTS_ERROR = 'Error! There is another project with that name already.';
-export const PROJECT_PATH = '/projects';
--- helperFunctions/createFiles.js
+++ helperFunctions/createFiles.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import {FOLDER_EXISTS_ERROR, PROJECT_PATH} from '../constants.js';
+import {FOLDER_EXISTS_ERROR} from '../constants.js';
 import createPackage from './createPackage.js';
 import createENV from './createEnv.js';
 import createServer from './createServer.js';
@@ -12,43 +12,51 @@ export const generateProject = (name, port, author, description, mongooseConnect
 	// check if a folder with the project name exists
 	try {
 		// if it exists
-		if (fs.existsSync(`.${PROJECT_PATH}/${name}`)) {
+		if (fs.existsSync(`./${name}`)) {
 			console.log(FOLDER_EXISTS_ERROR);
 		} else {
 			// if it doesn't exist, create the folder
-			fs.mkdir(`.${PROJECT_PATH}/${name}`, (err) => {
+			fs.mkdir(`./${name}`, (err) => {
 				if (err) throw err;
 			});
+
 			// create the .env file
-			fs.writeFileSync(`.${PROJECT_PATH}/${name}/.env`, createENV(mongooseConnection), (err) => {
+			fs.writeFileSync(`./${name}/.env`, createENV(mongooseConnection), (err) => {
 				if (err) throw err;
 			});
+
 			// create the package.json file
-			fs.writeFileSync(`.${PROJECT_PATH}/${name}/package.json`, createPackage(name, description, author), (err) => {
+			fs.writeFileSync(`./${name}/package.json`, createPackage(name, description, author), (err) => {
 				if (err) throw err;
 			});
+
 			// create the src folder
-			fs.mkdir(`.${PROJECT_PATH}/${name}/src`, (err) => {
+			fs.mkdir(`./${name}/src`, (err) => {
 				if (err) throw err;
 			});
+
 			// create the server.js file
-			fs.writeFileSync(`.${PROJECT_PATH}/${name}/src/server.js`, createServer(port), (err) => {
+			fs.writeFileSync(`./${name}/src/server.js`, createServer(port), (err) => {
 				if (err) throw err;
 			});
+
 			// create the app.js file
-			fs.writeFileSync(`.${PROJECT_PATH}/${name}/src/app.js`, createApp(), (err) => {
+			fs.writeFileSync(`./${name}/src/app.js`, createApp(), (err) => {
 				if (err) throw err;
 			});
+
 			// create the utils folder
-			fs.mkdir(`.${PROJECT_PATH}/${name}/src/utils`, (err) => {
+			fs.mkdir(`./${name}/src/utils`, (err) => {
 				if (err) throw err;
 			});
+
 			// create the config.js file
-			fs.writeFileSync(`.${PROJECT_PATH}/${name}/src/utils/config.js`, createConfig(), (err) => {
+			fs.writeFileSync(`./${name}/src/utils/config.js`, createConfig(), (err) => {
 				if (err) throw err;
 			});
+
 			// create the connection.js file
-			fs.writeFileSync(`.${PROJECT_PATH}/${name}/src/utils/connection.js`, createConnection(), (err) => {
+			fs.writeFileSync(`./${name}/src/utils/connection.js`, createConnection(), (err) => {
 				if (err) throw err;
 			});
 		}
